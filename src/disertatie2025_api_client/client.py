d3ae2f351e3b7158ab01fa5fa05b78761ce6b3e6
guceanu18 <guceanudaniel18@gmail.com>
2025-05-03 11:05:25 +0300

    feat: send command to a router

--- src/disertatie2025_api_client/client.py
+++ src/disertatie2025_api_client/client.py
@@ -1,10 +1,16 @@
 import requests
 
-BASE_URL = "http://127.0.0.1:8000/inventory/"
+BASE_URL = "http://127.0.0.1:8000"
+INVENTORY_URL = f'{BASE_URL}/inventory/'
+COMMANDS_URL = f'{BASE_URL}/commands/'
+CONFIG_URL = f'{BASE_URL}/config/'
 
 class RouterClient:
-    def __init__(self, base_url=BASE_URL):
+    def __init__(self, base_url=BASE_URL, inventory_url=INVENTORY_URL, commands_url=COMMANDS_URL, config_url=CONFIG_URL):
         self.base_url = base_url
+        self.inventory_url = inventory_url
+        self.commands_url = commands_url
+        self.config_url = config_url
 
     def add_router(self, name, mgmt_ip, site):
         data = {
@@ -12,38 +18,44 @@ class RouterClient:
             "mgmt_ip": mgmt_ip,
             "site": site
         }
-        response = requests.post(self.base_url, json=data)
-        response.raise_for_status()
+        response = requests.post(self.inventory_url, json=data)
         return response.json()
 
     def list_routers(self):
-        response = requests.get(self.base_url)
+        response = requests.get(self.inventory_url)
         response.raise_for_status()
         return response.json()
 
     def get_router_by_name(self, router_name):
-        url = f"{self.base_url}{router_name}"
+        url = f"{self.inventory_url}{router_name}"
         response = requests.get(url)
-        response.raise_for_status()
         return response.json()
 
     def delete_router(self, router_name):
-        url = f"{self.base_url}{router_name}"
+        url = f"{self.inventory_url}{router_name}"
         response = requests.delete(url)
         if response.status_code == 404:
             print(f"Router '{router_name}' not found.")
             return None
-        response.raise_for_status()
         return response.json()
 
     def update_router(self, router_name, **kwargs):
         # kwargs can include name, mgmt_ip, site, device_type
         if not kwargs:
             raise ValueError("You must provide at least one field to update.")
-        url = f"{self.base_url}{router_name}"
+        url = f"{self.inventory_url}{router_name}"
         response = requests.patch(url, json=kwargs)
         if response.status_code == 404:
             print(f"Router '{router_name}' not found.")
             return None
-        response.raise_for_status()
         return response.json()
+
+    def run_command(self, router_name, command):
+        url = f"{self.commands_url}run"
+        payload = {
+            "router_name": router_name,
+            "command": command
+        }
+        response = requests.post(url, json=payload)
+        response.raise_for_status()
+        return response.json()["output"]
--- src/disertatie2025_api_client/client_test.py
+++ src/disertatie2025_api_client/client_test.py
@@ -1,24 +1,26 @@
-from src.client.client import RouterClient
+from src.disertatie2025_api_client.client import RouterClient
 
 client = RouterClient()
 
 # Add a router
 print(client.add_router(
-    name="CE-R2",
-    mgmt_ip="192.168.1.2",
-    site="SiteB"
+    name="R_INTERNET",
+    mgmt_ip="192.168.72.31",
+    site="ISP"
 ))
 
 # List routers
 print(client.list_routers())
 
 # Get a router by name
-print(client.get_router_by_name("CE-R2"))
+print(client.get_router_by_name("R_INTERNET"))
 
 # Update a router
-print(client.update_router("CE-R2", site="SiteC"))
+print(client.update_router("R_INTERNET", site="ISP_PE"))
 
-# # Delete a router
-print(client.delete_router("CE-R2"))
+# Send a command to a router
+print(client.run_command("R_INTERNET", "show ip interface brief"))
 
+# Delete a router
+print(client.delete_router("R_INTERNET"))
 
